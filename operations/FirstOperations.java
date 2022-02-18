93cb20a762eb629ab49d4f58931d7f80e22ae4fb
ElenaPopova12 <leonoria@ukr.net>
2022-02-18 10:29:14 +0200

    firstOper

--- operations/FirstOperations.java
+++ operations/FirstOperations.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class FirstOperations {
+public class FirstOperations {
+    public static void main(String[] args) {
+        int i1 = 5;
+        int i2 = 10;
+        int i3 = 15;
+        int i4 = 20;
+        int bb1 = i1 + i2;
+        int bb2 = i3 + i4;
+        System.out.println(bb1 > bb2);
+
+        bb1 += 1;
+        bb2 -= 2;
+        System.out.println(bb1 > bb2);
+        System.out.println(bb1 % 2 == 0 || bb2 % 2 == 0);
+    }
 }
